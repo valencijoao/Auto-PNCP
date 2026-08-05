f6e712c39c204dd0c67302ff4dd5ece5c1240ed4
João Valenci <valencijoao30@gmail.com>
2026-08-05 18:32:53 -0300

    fix: ajuste na url  base para construir endpoints.

--- ferramentas/cliente_api.py
+++ ferramentas/cliente_api.py
@@ -2,11 +2,7 @@ import requests
 import json
 from pathlib import Path
 
-BASE_URL = "https://pncp.gov.br/api/pncp"
-PASTA_DADOS = Path("dados")
-PASTA_DADOS.mkdir(exist_ok=True)
-
-from explorar_swagger import carregar_swagger
+BASE_URL = "https://pncp.gov.br/api/"
 
 
 
@@ -39,36 +35,20 @@ def executar_endpoint(endpoint, path=None, query=None, metodo="GET"):
     return None
 
 
-def salvar_json(nome, dados):
-    """
-    Salva os dados recuperados dos endpoints em um arquivo JSON.
-    """
-
-    caminho = PASTA_DADOS / nome
-
-    with open(caminho,"w",encoding="utf-8") as f:
-        json.dump(
-            dados,
-            f,
-            indent=4,
-            ensure_ascii=False
-        )
-
-
-
 if __name__ == "__main__":
 
-    swagger = carregar_swagger("consulta")
+    
 
 
 
 
-    dados = executar_endpoint("/v1/orgaos/{cnpj}/compras/{ano}/{sequencial}/itens", path={
-        "cnpj":"06553937000170",
+    dados = executar_endpoint("/v1/orgaos/{cnpj}/compras/{ano}/{sequencial}/itens/{numeroItem}/resultados", path={
+        "cnpj":"92457217000143",
         "ano":"2026",
-        "sequencial":64
+        "sequencial":25,
+        "numeroItem":1
     })
 
-    salvar_json("itens_compra.json",dados)
+
 
 
