8018edc13b72e634a880df2608c389bd8d49b8c3
João Valenci <valencijoao30@gmail.com>
2026-08-07 14:36:01 -0300

    feat: inclusão de um minerador de domíminios, que percorre um JSON e salva novos domínios encontrados.

--- ferramentas/gerar_dominios.py
+++ ferramentas/gerar_dominios.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 from ferramentas.cliente_api import executar_endpoint
 
 ARQUIVO = Path("dados/dominios.json")
+PASTA_COMPRAS = Path("dados/compras")
 
 def carregar_dominios():
     """
@@ -14,7 +15,7 @@ def carregar_dominios():
     if ARQUIVO.exists():
 
         with open(ARQUIVO, encoding="utf=8") as f:
-            return json.load()
+            return json.load(f)
 
     return{}
 
@@ -55,8 +56,125 @@ def atualizar_dominios(nome, endpoint):
     print("OK")
 
 
+def normalizar_nomes(nome):
+    """
+    Remove os sufixos presentes nos domínios encontrados.
+    """
+
+    for sufixo in (
+        "id",
+        "Nome"
+    ):
+        if nome.endswith(sufixo):
+            return nome[:-len(sufixo)]
+
+    return nome
+
+
+def registrar_par(dominios, dominio, codigo, descricao):
+    """
+    Registra um par código -> descrição dentro de um domínio.
+    """
+
+    dominio = normalizar_nomes(dominio)
+
+    if dominio not in dominios:
+        dominios[dominio] = {}
+
+    dominios[dominio][str(codigo)] = descricao
+
+
+def extrair_pares(objeto, dominios, pai=None):
+    """
+    Percorre o JSON e mapeia pares de nomes e códigos.
+    """
+
+    if isinstance(objeto, dict):
+
+
+        if(
+            pai is not None
+            and "codigo" in objeto
+            and "nome" in objeto
+        ):
+            registrar_par(
+                dominios,
+                pai,
+                objeto["codigo"],
+                objeto["nome"]
+            )
+
+    
+        for chave in objeto:
+
+            if chave.endswith("Id"):
+                            
+
+                nome = chave[:-2]
+                chave_nome = nome + "Nome"
+
+                if chave_nome in objeto:
+
+                    registrar_par(
+                        dominios,
+                        nome,
+                        objeto[chave],
+                        objeto[chave_nome]
+                        )
+
+            valor = objeto[chave]
+
+            extrair_pares(
+                valor,
+                dominios,
+                pai=chave
+            )
+
+            
+    elif isinstance(objeto, list):
+            
+        for item in objeto:
+
+            extrair_pares(
+                item,
+                dominios,
+                pai=pai
+            )
+           
+    else:
+
+        return
+
+
+def minerar_arquivo(caminho, dominios):
+    """
+    Extrai todos os domínios de um JSON.
+    """
+
+    with open(caminho, encoding="utf-8") as f:
+        dados = json.load(f)
+
+    dominios = carregar_dominios()
+
+    extrair_pares(
+        dados,
+        dominios
+    )
+
+    salvar_dominios(dominios)
+
+def minerar_pasta():
+    """
+    Extrai os domínios de todos o JSON de um pasta.
+    """
+
+    dominios = carregar_dominios()
+
+
+
+
 if __name__ == "__main__":
 
-    carregar_dominios()
-    salvar_dominios()
-    atualizar_dominios()
+    minerar_arquivo(
+        Path(r"dados\compras\39215827000158-2026-8\itens.json")
+        )
