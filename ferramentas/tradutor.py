6c5e26a299b45da99120986d5273a257d5c07da2
João Valenci <valencijoao30@gmail.com>
2026-08-07 16:27:43 -0300

    feat: função que percorre e traduz todo um arquivo json objeto por objeto. necessita revisão!.

--- ferramentas/tradutor.py
+++ ferramentas/tradutor.py
@@ -4,6 +4,7 @@ def traduzir(dominio, codigo):
     """
     Traduz um único código do domínio correspondente.
     """
+
     dominios = carregar_dominios()
 
     codigo = str(codigo)
@@ -16,8 +17,55 @@ def traduzir(dominio, codigo):
     
     return pares.get(codigo)
 
+
+def traduzir_objeto(objeto):
+    """
+    Traduz todos os códigos encontrados em  um JSON.
+    """
+
+    if isinstance(objeto, dict):
+
+        for chave in objeto:
+
+            if chave.endswith("Id"):
+
+                nome = chave[:-2]
+
+                descricao = traduzir(
+                    nome,
+                    objeto[chave]
+                )
+            
+                if descricao is not None:
+
+                    objeto[nome + "Nome"] = descricao
+
+            valor = objeto[chave]
+
+            traduzir_objeto(valor)
+
+    elif isinstance(objeto, list):
+
+        for item in objeto:
+
+            traduzir_objeto(item)
+
+    else:
+
+        return
+
+
 if __name__ == "__main__":
-    print(traduzir('modalidade', 6))
+
+    objeto = {
+        "modalidadeId": 6,
+        "modoDisputaId": 1,
+        "criterioJulgamentoId": 1
+}
+
+traduzir_objeto(objeto)
+
+print(objeto)
     
 
 
